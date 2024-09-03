1350727ab85665cc0d3a8a555322b84006f8782b
nelson lassa <129919174+NelsonLassa@users.noreply.github.com>
2024-09-03 17:21:23 -0300

    Funcion de body y estilos

--- JavaScript/E-Commerce2024/client/js/cart.js
+++ JavaScript/E-Commerce2024/client/js/cart.js
@@ -4,28 +4,59 @@ const modalContainer = document.getElementById("modal-container");
 const cartBtn = document.getElementById("cart-btn");
 
 const displayCart = () => {
-    modalContainer.innerHTML = "";
-    modalContainer.style.display = "block";
-    modalOverlay.style.display = "block"
-    // Modal Header
-    const modalHeder = document.createElement("div");
+  modalContainer.innerHTML = "";
+  modalContainer.style.display = "block";
+  modalOverlay.style.display = "block";
+  // Modal Header
+  const modalHeder = document.createElement("div");
 
-    const modalClose = document.createElement("div");
-    modalClose.innerText = "❌";
-    modalClose.className = "modal-close";
-    modalHeder.append(modalClose);
+  const modalClose = document.createElement("div");
+  modalClose.innerText = "❌";
+  modalClose.className = "modal-close";
+  modalHeder.append(modalClose);
 
-    modalClose.addEventListener("click", () => {
-        modalContainer.style.display = "none";
-        modalOverlay.style.display = "none";
-    })
+  modalClose.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+    modalOverlay.style.display = "none";
+  });
 
-    const modalTitlte = document.createElement("div");
-    modalTitlte.innerText = "Carrito";
-    modalTitlte.className = "modal-title";
-    modalHeder.append(modalTitlte);
+  const modalTitlte = document.createElement("div");
+  modalTitlte.innerText = "Carrito";
+  modalTitlte.className = "modal-title";
+  modalHeder.append(modalTitlte);
 
-    modalContainer.append(modalHeder);
-}
+  modalContainer.append(modalHeder);
 
+  // Modal Body
+  cart.forEach((productos) => {
+    const modalBody = document.createElement("div");
+    modalBody.className = "modal-body";
+    modalBody.innerHTML = `
+        <div class="product">
+                <img class="product-img" src="${productos.img}"/>
+                <div class="product-info">
+                    <h4>${productos.productName}
+                </div>
+            <div class="quantity">
+                <span class="quantity-btn-decrese">-</span>
+                <span class="quantity-input">${productos.quanty}</sapn>
+                <span class="quantity-btn-increse">+</span>
+            </div>
+                <div class="price">${productos.price * productos.quanty} $</div>
+                <div class="delete-product">❌</div>
+        </div>
+        `;
+    modalContainer.append(modalBody);
+  });
+
+  // modal footer
+
+  const modalFooter = document.createElement("div");
+  modalFooter.className = "modal-footer";
+  modalFooter.innerHTML = `
+    <div class="total-price"> Totla :)</div>
+    `;
+
+  modalContainer.append(modalFooter);
+};
 cartBtn.addEventListener("click", displayCart);
