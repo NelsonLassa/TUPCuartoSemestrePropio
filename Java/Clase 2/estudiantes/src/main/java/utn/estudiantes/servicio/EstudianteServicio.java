a85d8ef6037e044f17830260a5a28526e8b5f607
nelson lassa <nelsonlassa63@gmail.com>
2024-09-18 18:23:57 -0300

    Clase 5 java spring

--- Java/Clase 2/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Java/Clase 2/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -2,7 +2,7 @@ package utn.estudiantes.servicio;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.modelo.Estudiantes2024;
 import utn.estudiantes.repositorio.EstudianteRepositorio;
 
 import java.util.List;
@@ -15,24 +15,24 @@ public class EstudianteServicio implements IEstudianteServicio{
 
     // TODO esto no es sobreescritura es implementación
     @Override
-    public List<Estudiante> listarEstudiantes() { //Devolvemos la lista completa de los estudiantes
-        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+    public List<Estudiantes2024> listarEstudiantes() { //Devolvemos la lista completa de los estudiantes
+        List<Estudiantes2024> estudiantes = estudianteRepositorio.findAll();
         return estudiantes;
     }
 
     @Override
-    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {// devuelve el id o null
-        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
+    public Estudiantes2024 buscarEstudiantePorId(Integer idestudiantes2024) {// devuelve el id o null
+        Estudiantes2024 estudiante = estudianteRepositorio.findById(idestudiantes2024).orElse(null);
         return estudiante;
     }
 
     @Override
-    public void guardarEstudiante(Estudiante estudiante) {
+    public void guardarEstudiante(Estudiantes2024 estudiante) {
         estudianteRepositorio.save(estudiante);
     }
 
     @Override
-    public void eliminarEstudiante(Estudiante estudiante) {
+    public void eliminarEstudiante(Estudiantes2024 estudiante) {
         estudianteRepositorio.delete(estudiante);
     }
 }
